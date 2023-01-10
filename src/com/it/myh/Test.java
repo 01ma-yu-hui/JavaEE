aea0d4e0c2a677f6de5354f36f05182c6bb289c5
myh <2722407149@qq.com>
2023-01-10 17:46:12 +0800

    init mas

---
+++ src/com/it/myh/Test.java
@@ -0,0 +1,11 @@
+package com.it.myh;
+
+/**
+ * @Description: // 类说明，在创建类时要填写
+ * @ClassName: Test    // 类名，会自动填充
+ * @Author: MYH          // 创建者
+ * @Date: 2023/1/10 17:45   // 时间
+ * @Version: 1.0     // 版本
+ */
+public class Test {
+}
