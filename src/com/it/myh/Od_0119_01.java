5bf8ef93642ec8aac08f586a97a7de557862d3a2
myh <2722407149@qq.com>
2023-01-19 22:33:39 +0800

    01.19号完成了一节Java基础课程，有点拉！！

---
+++ src/com/it/myh/Od_0119_01.java
@@ -0,0 +1,16 @@
+package com.it.myh;
+
+
+/**
+ * @Description: // 类说明，在创建类时要填写
+ * @ClassName: Od_0119_01    // 类名，会自动填充
+ * @Author: MYH          // 创建者
+ * @Date: 2023/1/19 22:20   // 时间
+ * @Version: 1.0     // 版本
+ */
+public class Od_0119_01 {
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        System.out.println(s1.length());
+    }
+}
