cbac75162eff84fd58292c7c1cc3cfeb69b155b4
myh <2722407149@qq.com>
2023-01-27 21:50:17 +0800

    01.27号完成了String的常用方法！

---
+++ src/com/it/myh/Od_0127_01.java
@@ -0,0 +1,19 @@
+package com.it.myh;
+
+/**
+ * @Description: // 类说明，在创建类时要填写
+ * @ClassName: Od_0127_01    // 类名，会自动填充
+ * @Author: MYH          // 创建者
+ * @Date: 2023/1/27 21:00   // 时间
+ * @Version: 1.0     // 版本
+ */
+public class Od_0127_01 {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        System.out.println(s1.charAt(4));
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.concat("world"));
+        System.out.println(s1.substring(2));
+        System.out.println(s1.substring(2,3));
+    }
+}
